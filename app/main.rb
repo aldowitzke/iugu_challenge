cc6a9bf2bfbdd43c5ae93218c230669594eb387d
Aldo <aldo_mwa@hotmail.com>
2020-03-19 13:41:29 -0300

    add output for main file

--- app/main.rb
+++ app/main.rb
@@ -2,6 +2,7 @@ require 'active_record'
 require 'pry'
 require_relative 'account_csv'
 require_relative 'transaction_csv'
+require_relative 'account_movement'
 
 def db_configuration
   db_configuration_file = File.join(File.expand_path('..', __FILE__), '..', 'db', 'config.yml')
@@ -10,5 +11,16 @@ end
 
 ActiveRecord::Base.establish_connection(db_configuration["development"])
 
-AccountCsv.new(ARGV[0]).call
-TransactionCsv.new(ARGV[1]).call
+Account.destroy_all
+Transaction.destroy_all
+
+AccountCsv.call(ARGV[0])
+TransactionCsv.call(ARGV[1])
+
+Transaction.all.each do |transaction|
+  AccountMovement.call(transaction)
+end
+
+Account.all.each do |account|
+  puts "Account number: #{account.id} | Balance: #{account.balance}"
+end
