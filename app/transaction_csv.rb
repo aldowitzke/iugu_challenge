3e26b400ccc992d491f045bec9cfaeb6ede3779c
Aldo <aldo_mwa@hotmail.com>
2020-03-19 11:49:54 -0300

    add transaction class for saving csv

---
+++ app/transaction_csv.rb
@@ -0,0 +1,33 @@
+require 'csv'
+require 'pry'
+require 'active_record'
+require_relative '../models/Transaction'
+
+class TransactionCsv
+  attr_reader :csv, :transactions
+
+  def initialize(csv)
+    @csv = csv
+  end
+
+  def self.call(*params)
+    new(*params).call
+  end
+
+  def call
+    open_transaction_csv
+    save_transactions
+  end
+
+  private
+
+  def open_transaction_csv
+    @transactions = CSV.read(csv)
+  end
+
+  def save_transactions
+    transactions.each do |transaction|
+      Transaction.new(account_id: transaction[0], value: transaction[1]).save
+    end
+  end
+end
